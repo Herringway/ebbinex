456b779a075573a0878a21e4fa720e972b90fe9f
Cameron Ross <elpenguino@gmail.com>
2018-09-15 23:55:36 -0300

    add some basic multiregion support (mother 2 not well supported yet)

---
+++ source/common.d
@@ -0,0 +1,8 @@
+module common;
+
+struct DumpInfo {
+	string subdir;
+	string name;
+	ulong offset;
+	ulong size;
+}
---
+++ source/jpn.d
@@ -0,0 +1,11 @@
+module jpn;
+
+import common;
+
+immutable DumpInfo[] jpnEntries = [
+    DumpInfo("text_data", "0", 0x50284, 0xFD7C),
+    DumpInfo("text_data", "1", 0x60000, 0x10000),
+    DumpInfo("text_data", "2", 0x70000, 0x10000),
+    DumpInfo("text_data", "3", 0x80000, 0x10000),
+    DumpInfo("text_data", "4", 0x90000, 0xE514),
+];
